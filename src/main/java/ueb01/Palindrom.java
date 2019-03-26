afaae499c28b8a43033f3470275ee11aa4729877
sWMAqudief <38352785+sWMAqudief@users.noreply.github.com>
2019-03-26 13:13:47 +0100

    oop01

--- src/main/java/ueb01/Palindrom.java
+++ src/main/java/ueb01/Palindrom.java
@@ -1,9 +1,28 @@
 package ueb01;
 
+import java.util.Arrays;
+
 class Palindrom {
 	static boolean istPalindrom(String s) {
 		// Verwenden Sie die Klasse CharStackImpl um auf Palindrom zu testen!
-
-		return false;
+		if(s.length() == 1)
+		{
+			return true;
+		}
+		CharStackImpl stack = new CharStackImpl();
+		s = s.replaceAll(" ", "");
+		s = s.toLowerCase();
+		char[] array = s.toCharArray();
+		for(int k = 0; k < array.length; k++)
+		{
+			stack.push(array[k]);
+		}
+		int laenge = stack.size();
+		char[] vonhinten = new char[laenge];
+		for(int j = 0; j < vonhinten.length; j++)
+		{
+			vonhinten[j] = stack.pop();
+		}
+		return Arrays.equals(array, vonhinten);
 	}
 }
