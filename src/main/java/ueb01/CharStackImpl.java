b31577efcf1e4801e627762f565bb641a700edfb
sWMAqudief <38352785+sWMAqudief@users.noreply.github.com>
2019-03-26 12:35:50 +0100

    oop01

--- src/main/java/ueb01/CharStackImpl.java
+++ src/main/java/ueb01/CharStackImpl.java
@@ -1,4 +1,66 @@
 package ueb01;
 
-class CharStackImpl implements CharStack {
+import java.util.NoSuchElementException;
+
+class IntElement
+{
+    char value;
+    IntElement next;
+    IntElement(char v, IntElement e)
+    {
+        value = v;
+        next = e;
+    }
+}
+
+class CharStackImpl implements CharStack
+{
+    private IntElement head;
+    public void push(char c)
+    {
+        IntElement it = head;
+        if(head == null)
+        {
+            head = new IntElement(c, null);
+            return;
+        }
+
+        while(it.next != null)
+        {
+            it = it.next;
+        }
+        it.next = new IntElement(c, null);
+    }
+    public char pop()
+    {
+        IntElement it = head;
+        IntElement parent = head;
+        if(head == null)
+        {
+            throw new NoSuchElementException();
+        }
+        if(head.next == null)
+        {
+            head = null;
+        }
+        while(it.next != null)
+        {
+            parent = it;
+            it = it.next;
+        }
+        char x = it.value;
+        parent.next = null;
+        return x;
+    }
+    public int size()
+    {
+        int n = 0;
+        IntElement it = head;
+        while(it != null)
+        {
+            n++;
+            it = it.next;
+        }
+        return n;
+    }
 }
